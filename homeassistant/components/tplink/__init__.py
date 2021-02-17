5b4510f7c2999fb26abcd60122d72025c2903a9a
Angelo Gagliano <25516409+TheGardenMonkey@users.noreply.github.com>
2021-02-16 20:55:37 -0500

    Revert to clear in unload

--- homeassistant/components/tplink/__init__.py
+++ homeassistant/components/tplink/__init__.py
@@ -121,17 +121,17 @@ async def async_setup_entry(hass: HomeAssistantType, config_entry: ConfigType):
     return True
 
 
-async def async_unload_entry(hass, entry):
+async def async_unload_entry(hass, config_entry):
     """Unload a config entry."""
     unload_ok = all(
         await asyncio.gather(
             *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in PLATFORMS
+                hass.config_entries.async_forward_entry_unload(config_entry, platform)
+                for platform in PLATFORMS
             ]
         )
     )
     if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
+        hass.data[DOMAIN].clear()
 
     return unload_ok
